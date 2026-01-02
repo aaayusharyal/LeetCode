8e794b541b0ee53f713155dc427fc4e2b33c4f7a
Aayush Aryal <aryalayush86@gmail.com>
2026-01-02 17:16:56 +0545

    Update 268.MissingNumber.cpp

--- Maths/268.MissingNumber.cpp
+++ Maths/268.MissingNumber.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int sum = (n*(n+1))/2;
-        int qsum = 0;
+        int sum = (n*(n+1))/2; //givbes the sum upto nth term
+        int qsum = 0; // question ko sum nikalne,initially 0
         for(int index=0; index<n; index++)
         {
              qsum = qsum + nums[index];
         }
-           return (sum - qsum);
+           return (sum - qsum);// diff of sum annd question ko sum
     }
     
 };
