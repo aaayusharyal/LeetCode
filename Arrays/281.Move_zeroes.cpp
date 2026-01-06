9d6ea7d398cb5e56f61f94b44ee9f0abfc6510bb
Aayush Aryal <aryalayush86@gmail.com>
2026-01-06 16:59:41 +0545

    Create 281.Move_zeroes.cpp

---
+++ Arrays/281.Move_zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int nonzeroes = 0;
+        for(int j=0; j<nums.size();j++)
+        {
+            if(nums[j]!=0)
+            {
+                swap(nums[nonzeroes], nums[j]);
+                nonzeroes++;
+            }
+        }
+    }
+};
